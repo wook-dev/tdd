4fac628b5c2026c2dae01587ce732302d728bcc8
wook <wook@station3.co.kr>
2020-12-16 15:03:20 +0900

    Update 컨트롤러 테스트

--- src/test/java/com/example/tdd/controller/BbsControllerTest.java
+++ src/test/java/com/example/tdd/controller/BbsControllerTest.java
@@ -1,11 +1,11 @@
 package com.example.tdd.controller;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -38,12 +37,11 @@ public class BbsControllerTest {
     mockMvc
         .perform(
             get("/bbs")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .param("page", "1")
                 .param("size", "10")
         )
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$", hasSize(0)))
+        .andExpect(jsonPath("$.content", Matchers.hasSize(0)))
         .andDo(print());
   }
 }
